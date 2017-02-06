fae7bbc9aef772c97d32dc88e6041c51d226f6bb
theodore.kim <taehyunk@buffalo.edu>
2017-02-06 01:37:18 -0500

    Assignment 11: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -28,10 +28,25 @@ var albumMarconi = {
     ]
 };
 
+var albumNumbers = {
+    title: 'The Numbers',
+    artist: 'Isaac Newton',
+    label: 'Mathematics',
+    year: '1701',
+    albumArtUrl: 'assets/images/album_covers/06.png',
+    songs: [
+        { title: 'First Law', duration: '3:01' },
+        { title: 'Second Law', duration: '4:01' },
+        { title: 'Third Law', duration: '2:21'},
+        { title: 'Gravity', duration: '4:14' },
+        { title: 'Apples', duration: '3:15'}
+    ]
+};
+
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
           '<tr class="album-view-song-item">'
-        + '  <td class="song-item-number">' + songNumber + '</td>'
+        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
         + '  <td class="song-item-title">' + songName + '</td>'
         + '  <td class="song-item-duration">' + songLength + '</td>'
         + '</tr>'
@@ -40,7 +55,13 @@ var createSongRow = function (songNumber, songName, songLength) {
         return template;
 };
 
-var setCurrentAlbum = function(album) {
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function (album) {
     // #1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
@@ -60,6 +81,32 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-window.onload = function() {
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class ="ion-play"></span></a>';
+
+window.onload = function () {
     setCurrentAlbum(albumPicasso);
-};
+
+    var albums = [albumMarconi, albumNumbers, albumPicasso];
+    var index = 0;
+    albumImage.addEventListener("click", function (event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == albums.length) {
+            index = 0;
+        }
+    });
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function (event) {
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+}
