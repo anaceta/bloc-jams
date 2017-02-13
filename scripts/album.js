99c80eea0075daf384b64c7ad464fb02475bc177
Theodore Kim <taehyunk@buffalo.edu>
2017-02-12 23:22:39 -0500

    Checkpoint 12: Completed

--- scripts/album.js
+++ scripts/album.js
@@ -46,7 +46,7 @@ var albumNumbers = {
 var createSongRow = function (songNumber, songName, songLength) {
     var template =
           '<tr class="album-view-song-item">'
-        + '  <td class="song-item-number">' + songNumber + '</td>'
+        + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
         + '  <td class="song-item-title">' + songName + '</td>'
         + '  <td class="song-item-duration">' + songLength + '</td>'
         + '</tr>'
@@ -83,7 +83,16 @@ var setCurrentAlbum = function (album) {
 
 window.onload = function () {
     setCurrentAlbum(albumPicasso);
-    
+
+var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
+var songRows = document.getElementsByClassName('album-view-song-item');
+
+// Album button templates
+var playButtonTemplate = '<a class="album-song-button"><span class ="ion-play"></span></a>';
+
+window.onload = function () {
+    setCurrentAlbum(albumPicasso);
+
     var albums = [albumMarconi, albumNumbers, albumPicasso];
     var index = 0;
     albumImage.addEventListener("click", function (event) {
@@ -94,3 +103,16 @@ window.onload = function () {
         }
     });
 };
+
+    songListContainer.addEventListener('mouseover', function(event) {
+        if (event.target.parentElement.className === 'album-view-song-item') {
+            event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
+        }
+    });
+    for (var i = 0; i < songRows.length; i++) {
+        songRows[i].addEventListener('mouseleave', function (event) {
+            this.children[0].innerHTML = this.children[0].getAttribute('data-song-number');
+        });
+    }
+}
+
