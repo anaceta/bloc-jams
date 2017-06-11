b9c11d515946ca659b29e6cbc8235f2a2b412991
Theodore Kim <taehyunk@buffalo.edu>
2017-06-11 13:21:14 -0400

    Checkpoint 18 v2

--- scripts/album.js
+++ scripts/album.js
@@ -118,7 +118,7 @@ var setCurrentAlbum = function (album) {
 var findParentByClassName = function (element, targetClass) {
     if (element) {
         var currentParent = element.parentElement;
-        while (currentParent.className != targetClass && currentParent.className !== null) {
+        if (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
